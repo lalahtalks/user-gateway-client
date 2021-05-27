d66c1936ee1c9f7d4af49a540095ef02a2a36a56
dlesignac <damien.lesignac@dreev.com>
2021-05-27 23:58:00 +0200

    update problem type

--- user-gateway-client-http-contract/src/main/java/io/lalahtalks/user/gateway/client/http/contract/UserGatewayProblemType.java
+++ user-gateway-client-http-contract/src/main/java/io/lalahtalks/user/gateway/client/http/contract/UserGatewayProblemType.java
@@ -3,6 +3,7 @@ package io.lalahtalks.user.gateway.client.http.contract;
 import io.lalahtalks.spring.problem.ProblemType;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.zalando.problem.Status;
 
 import java.net.URI;
 
@@ -13,12 +14,12 @@ public enum UserGatewayProblemType implements ProblemType {
     ACCOUNT_ALREADY_EXISTS(
             URI.create("urn:lalahtalks:problem:user-gateway:account-already-exists"),
             "Account already exists",
-            409),
+            Status.CONFLICT),
 
     ;
 
     private final URI type;
     private final String title;
-    private final int httpStatusCode;
+    private final Status httpStatus;
 
 }
